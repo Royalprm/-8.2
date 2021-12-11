b3361cac34aa27a9fa7fd48b768d531374f05802
Royalprm <95142405+Royalprm@users.noreply.github.com>
2021-12-11 18:15:01 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Упражнение_8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter f1 = new StreamWriter("D:\\f1.txt");
+            Random r = new Random();
+            for (int i = 0; i < 10; i++) f1.WriteLine(r.Next(0, 100));
+            f1.Close();
+            StreamReader fr = new StreamReader("D:\\f1.txt");
+            string[] strNumbers = System.IO.File.ReadAllLines(@"D:\\f1.txt");
+            int count = 0;
+            foreach (string strNumber in strNumbers)
+            {
+                if (Int32.TryParse(strNumber, out int number))
+                {
+                    count = count + number;
+                }
+            }
+            Console.WriteLine("Total : " + count);
+
+            Console.WriteLine();
+            Console.Write("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+}
